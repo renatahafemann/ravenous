405c93c587d2835e05950ba569879fed10371b04
Renata Hafemann <mgs.renata@gmail.com>
2023-12-12 16:55:12 -0500

    Edited sort options layout and theme

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/system/Box";
 import Grid from "@mui/material/Grid";
 import searchBackground2 from "../../images/searchBackground2.jpg";
@@ -7,75 +7,93 @@ import InputBase from "@mui/material/InputBase";
 import Typography from "@mui/material/Typography";
 import { ThemeProvider } from "@mui/material/styles";
 import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
 import { theme } from "../../theme/Theme";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [location, setLocation] = useState("");
+  const [sortBy, setSortBy] = useState("best_match");
+
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const handleChange = (event, newValue) => {
+    setSortBy(newValue);
+  };
+
   return (
     <ThemeProvider theme={theme}>
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      sx={{
-        height: "40vh",
-        backgroundImage: `url(${searchBackground2})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        marginBottom: 4,
-      }}
-    >
-      <Grid
-        container     
-        sx={{ justifyContent: "space-around", width: "40vw" }}
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          height: "40vh",
+          backgroundImage: `url(${searchBackground2})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          marginBottom: 4,
+        }}
       >
-          <Grid item >
-            <Typography >Best<br />Match</Typography>
+        <Tabs value={sortBy} onChange={handleChange} centered>
+          <Tab value="best_match" label="Best Match" />
+          <Tab value="rating" label="Highest Rated" />
+          <Tab value="review_count" label="Most Reviewed" />
+        </Tabs>
+
+        <Grid
+          container
+          spacing={2}
+          sx={{ justifyContent: "center", marginTop: 2 }}
+        >
+          <Grid item>
+            <Paper
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                width: 400,
+              }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search business"
+                onChange={handleSearchTermChange}
+              />
+            </Paper>
           </Grid>
-          <Grid item >
-            <Typography>Highest<br />Rated</Typography>
+          <Grid item>
+            <Paper
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                width: 400,
+              }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Where?"
+                onChange={handleLocationChange}
+              />
+            </Paper>
           </Grid>
-          <Grid item >
-            <Typography>Most<br />Reviewed</Typography>
-          </Grid>
-        <Grid item xs={12} md={10}>
-          <hr />
         </Grid>
-      </Grid>
-      <Grid
-        container
-        spacing={2}
-        sx={{ justifyContent: "center", marginTop: 2 }}
-      >
-        <Grid item >
-          <Paper
-            component="form"
-            sx={{
-              p: "2px 4px",
-              display: "flex",
-              alignItems: "center",
-              width: 400,
-            }}
-          >
-            <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search business" />
-          </Paper>
-        </Grid>
-        <Grid item>
-          <Paper
-            component="form"
-            sx={{
-              p: "2px 4px",
-              display: "flex",
-              alignItems: "center", 
-              width: 400                  
-            }}
-          >
-            <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Where?" />
-          </Paper>
-        </Grid>
-      </Grid>
-      <Button variant="contained" color="gold" sx={{m: 4}}>Let's Go</Button>
-    </Box>
+        <Button variant="contained" color="gold" sx={{ m: 4 }} onClick={() => alert(sortBy)}>
+          Let's Go
+        </Button>
+      </Box>
     </ThemeProvider>
   );
 };
--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,20 +1,41 @@
-import { createTheme} from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
-    typography: {
-      body1: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center",
-      }, 
-      gold: {
-        color: "#CAAE0C",
-      }
+  typography: {
+    body1: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center",
     },
-    palette: {
-      gold: {
-        main: "#CAAE0C",
-        contrastText: "#fff",
+    gold: {
+      color: "#CAAE0C",
+    },
+  },
+  components: {
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          color: "white",
+          fontWeight: "bold",
+          '&.Mui-selected': {
+            color: '#CAAE0C',
+          }
+        },
       },
     },
-  });
+    MuiTabs: {
+      styleOverrides: {
+        indicator: {
+          backgroundColor: "#CAAE0C",
+        },
+        }, 
+        
+      },
+  },
+  palette: {
+    gold: {
+      main: "#CAAE0C",
+      contrastText: "#fff",
+    },
+  },
+});
